36bafcfea73df133698590196a8ec4dd3ecd4f18
noel <ahmczsy@gmail.com>
2021-05-25 19:32:00 +0800

    update

--- fib/main.go
+++ fib/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(coinChange([]int{1, 2, 5, 4}, 29))
+	fmt.Println(coinChange([]int{2}, 3))
 }
 
 func fib(n int) int {
@@ -40,5 +40,26 @@ func coinChange(coins []int, amount int) int {
 		}
 		final = int(math.Min(float64(final), float64(subRes+1)))
 	}
+	if final == math.MaxInt64 {
+		return -1
+	}
 	return final
+}
+
+func coinChangeDP(coins []int, amount int) int {
+	result := make([]int, amount+1)
+	result[0] = 0
+	for i := 0; i <= len(result); i++ {
+		for _, coin := range coins {
+			if i-coin < 0 {
+				continue
+			}
+			result[i] = int(math.Min(float64(result[i]), float64(1+result[i-coin])))
+		}
+	}
+	if result[amount] == amount+1 {
+		return -1
+	} else {
+		return result[amount]
+	}
 }
